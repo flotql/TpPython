7b42ac1dfa4a3e9904b50f98758a10fa4d1c68a2
Florent T <94051212+flotql@users.noreply.github.com>
2022-01-05 12:41:39 +0100

    V2 Inscription
    Ajout choix nombre d'inscription
    Ajout cas d'erreur

--- inscription.py
+++ inscription.py
@@ -2,18 +2,62 @@ import re
 from datetime import date
 from traits import *
 
+continuer = True
+
 # Donnees inscriptions
 
-nom = input("Ecrire le nom ici:\n")
-prenom = input("Ecrire le prenom ici:\n")
-nvYear,nvMonth,nvDay = input("Ecrire avec un espace l'année de naissance, le moi et le jour:\n").split(' ')
+while continuer:
+    # Nombre d'inscription
+    nb = input("Combien d'inscriptions devez-vous rentrer? \n")
+    try:
+        nb = int(nb)
+    except ValueError:
+        print("Rentrer le nombre en chiffre")
+    else:
+        continuer = False
+# Inscriptions
+for _ in range(nb):
+    # Nom / Prenom
+    nom = input("Ecrire le nom ici:\n")
+    prenom = input("Ecrire le prenom ici:\n")
+    erreur = True
+    while erreur:
+        switch = 0
+        # Date de naissance
+        nvYear = input("Ecrire l'annee de naissance:\n")
+        nvMonth = input("Ecrire le moi de naissance:\n")
+        nvDay = input("Ecrire le jour de naissance\n")
+        if len(nvYear) == 4:
+            erreur = False
+        else:
+            print("l'année doit comporter 4 chiffres")
+            erreur = True
+            switch = 1
+        try:
+            nvYear = int(nvYear)
+            nvMonth = int(nvMonth)
+            nvDay = int(nvDay)
+        except ValueError:
+            print("Indiquer la date de naissance en chiffre")
+        else:
+            erreur = False
+        if switch == 1:
+            erreur = True
+        # Definir age et categorie nouveau inscrit
+        nvCat = categorie(nvYear, nvMonth, nvDay)
+        print(nvCat)
+        # Creation adresse mail
+        nvEmail = eMail(prenom, nom)
+        print(nvEmail)
+        # suivant = input("avez-vous d'autres inscriptions? y/n \n") V3 !!!!!
+        # if suivant == "n":
+        #     continuer = False
 
-# Definir age et categorie nouveau inscrit
 
-nvCat = categorie(nvYear,nvMonth,nvDay)
-print(nvCat)
 
-# Creation adresse mail
 
-nvEmail = eMail(prenom,nom)
-print(nvEmail)
+
+
+
+
+
