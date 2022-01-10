b1e4c56bb5af7bc0d809b783b9bb48f6a18a6d9d
flotql <94051212+flotql@users.noreply.github.com>
2022-01-10 12:05:00 +0100

    ajout explication fonctions

--- traits.py
+++ traits.py
@@ -6,10 +6,12 @@ dt = date.today()
 tst = "inscrits-" + str(dt) + ".csv"
 headerList = ["Nom", "Prenom", "Categorie", "Mail"]
 
+# definir l'age de l'inscrit
 def age(bd):
     age = dt.year - bd.year - ((dt.month, dt.day) < (bd.month, bd.day))
     return age
 
+# ajout inscrit dans categorie
 def categorie(year,month,day):
     cat = {"Poussin": 12, "Cadet": 18, "Junior": 24, "Semi-pro": 30, "Pro": 40}
     nvAge = age(date(int(year), int(month), int(day)))
@@ -19,21 +21,21 @@ def categorie(year,month,day):
         if nvAge < list(cat.values())[ind]:
             return key
 
+# creation email
 def eMail(firstName,secondName):
     fName= firstName[0].upper()
     sName= secondName.lower()
     return '{}.{}@baton-rouge.fr'.format(fName,sName)
 
-
+# ajout d'inscrits dans le fichier csv
 def ajout(leMail,laCat,leNom,lePrenom):
-    with open(tst,"a") as file:
+    with open('inscriptions\\'+tst,"a") as file:
         informations = csv.writer(file, delimiter=';')
         informations.writerow([leNom,lePrenom,laCat,leMail])
 
-# verifier si le csv du jour est cree
+# verifier si le csv du jour est cree et creation si il n'existe pas
 def fichierCSV():
-    # Si oui ajouter les donnees en utilisant la fonction
-    if not os.path.isfile(tst):
-        with open(tst, "a") as file:
+    if not os.path.isfile('inscriptions\\'+tst):
+        with open('inscriptions\\'+tst, "a") as file:
             dw = csv.DictWriter(file, delimiter=';', fieldnames=headerList)
             dw.writeheader()
